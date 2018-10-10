814dabbd5c65fbde0d7d798500aaf9bd2f111aa9
Omar Villalpando <g_omar_v@live.com.mx>
2018-10-10 00:07:17 -0500

    cambios menores en definicion de NN

--- IrisSpecies/IrisSpecies.py
+++ IrisSpecies/IrisSpecies.py
@@ -33,18 +33,29 @@ inputs = tf.placeholder(tf.float32,[None, input_size])
 targets = tf.placeholder(tf.float32, [None, output_size])
 
 ################################LAYER_1#################################
-nets_1 = tf.get_variable("nets_1", [input_size, nets_size_layer_1] )
+weights_1 = tf.get_variable("weights_1", [input_size, nets_size_layer_1] )
 biases_net_1 = tf.get_variable("biases_1", [nets_size_layer_1])
+nets_1 = tf.matmul(inputs, weights_1) + biases_net_1
 ############################OUTPUT_LAYER_1##############################
-outputs_1 = tf.nn.relu( tf.matmul(inputs, nets_1) + biases_net_1)
+outputs_1 = tf.nn.relu( nets_1)
 
 
 ################################LAYER_2#################################
-nets_2 = tf.get_variable("nets_2", [nets_size_layer_1, nets_size_layer_2])
-biases_2 = tf.get_variable("biases_2", [nets_size_layer_2])
+weights_2 = tf.get_variable("weights_2", [nets_size_layer_1, nets_size_layer_2])
+biases_net_2 = tf.get_variable("biases_2", [nets_size_layer_2])
+nets_2 = tf.matmul(outputs_1, weights_2) + biases_net_2
 ############################OUTPUT_LAYER_2##############################
-outputs_2 = tf.nn.relu( tf.matmul(outputs_1, nets_2) + biases_2 )
+outputs_2 = tf.nn.relu( nets_2 )
 
+
+
+################################LAYER_3#################################
+weights_3 = tf.get_variable("weights_3", [nets_size_layer_2, nets_size_layer_3])
+biases_net_3 = tf.get_variable("biases_3", [nets_size_layer_3])
+nets_3 = tf.matmul(outputs_2, weights_3) + biases_net_3
+
+
+loss = tf.nn.softmax_cross_entropy_with_logits(logits=nets_3, labels=targets)
 
 
 
