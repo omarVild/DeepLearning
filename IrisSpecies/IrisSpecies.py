75f201d0851324a09d00309acc1b61dbed8d8bb4
Omar Villalpando <g_omar_v@live.com.mx>
2018-10-16 00:44:17 -0500

    Se cambia a numero la etiqueta target

--- IrisSpecies/IrisSpecies.py
+++ IrisSpecies/IrisSpecies.py
@@ -9,28 +9,34 @@ irisDataSetTotal =  pd.read_csv('dataSet/Iris.csv', sep="," )
 print (irisDataSetTotal.head())
 
 colnames = ['SepalLengthCm','SepalWidthCm','PetalLengthCm','PetalWidthCm']
-color_wheel = {'Iris-setosa': "#FF0000", 'Iris-versicolor': "#00FF00", 'Iris-virginica': "#0000FF"}
+color_wheel = ["#FF0000", "#00FF00", "#0000FF"]
+one_hot = [[1,0,0],[0,1,0],[0,0,1]]
 
-
-colors = irisDataSetTotal["Species"].map(lambda x: color_wheel.get(x))
+colors = irisDataSetTotal["Species"].map(lambda x: color_wheel[x-1])
 scatter_matrix(irisDataSetTotal[colnames], color = colors,  diagonal='kde', marker="*")
 
-plt.show()
 
-x_train, x_test, y_train, y_test = train_test_split(irisDataSetTotal[colnames], irisDataSetTotal['Species'], test_size=0.3)
-print('***************************')
-print(x_train)
+label_targets = irisDataSetTotal["Species"].map(lambda x: one_hot[x-1])
+
+#print("************label_targets*************************")
+#print(label_targets)
+
+#plt.show()
+
+x_train, x_test, y_train, y_test = train_test_split(irisDataSetTotal[colnames], label_targets, test_size=0.3)
+#print('***************************')
+#print(x_train)
 
 
 input_size = 4
-nets_size_layer_1 = 10
-nets_size_layer_2 = 10
-nets_size_layer_3 = 10
+nets_size_layer_1 = 3
+nets_size_layer_2 = 3
+nets_size_layer_3 = 3
 
-output_size = 4
+output_size = 3
 
-inputs = tf.placeholder(tf.float32,[None, input_size])
-targets = tf.placeholder(tf.float32, [None, output_size])
+inputs = tf.placeholder(tf.float32,  shape=[None, input_size])
+targets = tf.placeholder(tf.float32,  shape=[None, output_size])
 
 ################################LAYER_1#################################
 weights_1 = tf.get_variable("weights_1", [input_size, nets_size_layer_1] )
@@ -56,8 +62,56 @@ nets_3 = tf.matmul(outputs_2, weights_3) + biases_net_3
 
 
 loss = tf.nn.softmax_cross_entropy_with_logits(logits=nets_3, labels=targets)
+cross_entropy = tf.reduce_mean(loss)
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.5)
+cost= optimizer.minimize(cross_entropy)
+
+x_train, x_test, y_train, y_test = train_test_split(irisDataSetTotal[colnames].as_matrix(), irisDataSetTotal['Species'], test_size=0.3)
+
+#print('*************X train**************')
+#print(x_train)
+
+epochs_nums = 10000
+step = 1
+display_step=100
+
+init = tf.global_variables_initializer()
 
 
+out_equals_target = tf.equal(tf.argmax(nets_3, 1), tf.argmax(targets, 1))
+accuracy = tf.reduce_mean(tf.cast(out_equals_target, tf.float32))
+
+one_hot= [[1,0,0],[0,1,0],[0,0,1]]
+
+# Start training
+with tf.Session() as sess:
+
+    # Run the initializer
+    sess.run(init)
 
 
+    for epoch in range(epochs_nums):
+        for i in range(len(x_train)):
+            print("*************x_train[i: i+1]******************")
+            print( x_train[i: i+1].shape)
+            print( x_train[i: i+1])
+            
+            
+            print("*******************************+")
+            print(y_train[i: i + 1].shape)
+            print(y_train[i: i + 1])
+            print(y_train[i: i + 1].values)
+            print("*******************************+")
+            print(one_hot[y_train[i: i + 1].values[0]-1])
+            y_trainTMP = [one_hot[y_train[i: i + 1].values[0]-1]]
+            print(y_trainTMP)
+            print("*******************************+")
+            
+            
+            sess.run(cost, feed_dict={inputs: x_train[i: i+1], targets: y_trainTMP })
+            
+            #if step % display_step == 0 or step == 1:
+                
+              
 
